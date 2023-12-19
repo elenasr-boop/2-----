2b6ef7edb1e4c8bd2b15b001917ff1e37fd00b45
Elena Rybakova <lelkya84@gmail.com>
2023-12-19 08:50:43 +0300

    1-7 задание из дз

---
+++ hw1/hw2.js
@@ -0,0 +1,22 @@
+let a=10; //1 задание
+// alert(a);
+a=20;
+// alert(a);
+let b=2007; //2 задание
+// alert(b);
+let c="Брендан Эйх"; //3 задание
+// alert (c);
+let d=10; let e=2; //4 задание
+// alert (d+e); alert(d-e); alert(d*e); alert (d/e);
+const result=2**5; //5 задание
+// alert(result);
+a=9; b=2;  //6 задание
+// alert(a%b);
+let num=1;  //7 задание
+num=num+5;
+num=num-3;
+num=num*7;
+num=num/3;
+num=num+1;
+num=num-1;
+// alert (num);

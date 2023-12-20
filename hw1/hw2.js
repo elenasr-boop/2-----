23976e99c2ec0bb21fea2c7255f09682b658523a
Elena Rybakova <lelkya84@gmail.com>
2023-12-20 10:23:46 +0300

    8-10 задания

--- hw1/hw2.js
+++ hw1/hw2.js
@@ -1,17 +1,17 @@
 let a=10; //1 задание
-// alert(a);
+alert(a);
 a=20;
-// alert(a);
+alert(a);
 let b=2007; //2 задание
-// alert(b);
+alert(b);
 let c="Брендан Эйх"; //3 задание
-// alert (c);
+alert (c);
 let d=10; let e=2; //4 задание
-// alert (d+e); alert(d-e); alert(d*e); alert (d/e);
+alert (d+e); alert(d-e); alert(d*e); alert (d/e);
 const result=2**5; //5 задание
-// alert(result);
+alert(result);
 a=9; b=2;  //6 задание
-// alert(a%b);
+alert(a%b);
 let num=1;  //7 задание
 num=num+5;
 num=num-3;
@@ -19,4 +19,20 @@ num=num*7;
 num=num/3;
 num=num+1;
 num=num-1;
-// alert (num);
+alert (num);
+let age=prompt('Сколько вам лет?'); //8 задание
+alert(age);
+let user = { //9 задание
+    name: 'Vitya',
+    age: 34,
+    isAdmin: false
+};
+user['city of residence']='Moscow';
+alert(user['city of residence']);
+user.age=43;
+alert(user.age);
+delete user['city of residence'];
+let info=prompt('Какую информацию хотите узнать о пользователе?', 'name');
+alert(user[info]);
+let nominal=prompt('Как вас зовут?'); //10 задание
+alert('Привет, '+nominal+'!');
